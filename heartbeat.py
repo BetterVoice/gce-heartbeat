04e620ddf3c57f52741eac988c2faf901e3d94bf
thomasquintana <quintana.thomas@gmail.com>
2015-09-01 18:39:03 +0000

    Added branch for pgsql.

--- heartbeat.py
+++ heartbeat.py
@@ -6,6 +6,7 @@ from oauth2client.client import GoogleCredentials
 from subprocess import CalledProcessError, check_call
 
 import os
+import psycopg2
 import time
 
 class HeartBeat(object):
@@ -20,6 +21,10 @@ class HeartBeat(object):
     self.primary_zone = kwargs['primary_zone'][0]
     self.secondary = kwargs['secondary'][0]
     self.secondary_zone = kwargs['secondary_zone'][0]
+    self.db = kwargs['db'][0]
+    self.db_user = kwargs['db_user'][0]
+    self.db_password = kwargs['db_password'][0]
+    self.db_dir = kwargs['db_dir'][0]
     self.pri_nic = self.__get_network_interfaces__(self.project,
                                                    self.primary,
                                                    self.primary_zone)[0]
@@ -75,14 +80,25 @@ class HeartBeat(object):
       raise Exception(result['error'])
     return result['networkInterfaces']
 
-  def __ping__(self, address, interval):
-    check_call(['ping', '-c', '1', '-W', '1', address])
+  def __test__(self, host, user, password, db):
+    try:
+      parameters = "host='%s' user='%s' password='%s' dbname='%s'" % \
+                   (host, user, password, db)
+      connection = psycopg2.connect(parameters)
+      connection.close()
+    except psycopg2.Error as error:
+      raise RuntimeError()
 
   def start(self):
     while True:
       try:
-        self.__ping__(self.pri_nic['networkIP'], self.interval)
-      except CalledProcessError as error:
+        self.__test__(self.pri_nic['networkIP'], self.db_user,
+                      self.db_password, self.db)
+      except RuntimeError as error:
+        # Initiate PostgreSQL failover.
+        trigger = '%s/failover' % self.db_dir
+        with open(trigger, 'a'):
+          os.utime(trigger, None)
         # Make sure the address is not in use.
         address = self.__get_address__(self.project, self.region,
                                        self.address_name)
@@ -102,6 +118,7 @@ class HeartBeat(object):
         self.sec_nic = self.__get_network_interfaces__(self.project,
                                                        self.secondary,
                                                        self.secondary_zone)[0]
+        break
       time.sleep(self.interval)
 
 if __name__ == "__main__":
@@ -129,6 +146,16 @@ if __name__ == "__main__":
   parser.add_argument('--secondary-zone', dest = 'secondary_zone', nargs = 1,
                       required = True,
                       help = 'The secondary instance zone.')
+  parser.add_argument('--db', dest = 'db', nargs = 1,
+                      required = True, help = 'The database name.')  
+  parser.add_argument('--db-user', dest = 'db_user', nargs = 1,
+                      required = True,
+                      help = 'The database user name.')
+  parser.add_argument('--db-password', dest = 'db_password', nargs = 1,
+                      required = True,
+                      help = 'The database password.')
+  parser.add_argument('--db-dir', dest = 'db_dir', nargs = 1,
+                      required = True, help = 'The database directory.')
   args = parser.parse_args()
   # Start the hearbeat service.
   HeartBeat(**vars(args)).start()
